d1e0893b40d318912bdab9461b00336ca5e1f37f
Peter Odero <peterodero561@gmail.com>
2024-10-17 19:44:00 +0300

    build: creating trigers to decrease items if a new oder is added

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- creating a trigger
+DELIMITER //
+CREATE TRIGGER decrease_item_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
+END; //
+DELIMITER ;
