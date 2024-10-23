60ef9af3c599a74d970b1cb24dc7962f7f1a8526
Peter Odero <peterodero561@gmail.com>
2024-10-24 02:39:18 +0300

    fix2:

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     '''changes all topics of a school document based on the name'''
-    mongo_collection.updateMany(
+    mongo_collection.update_many(
             {'name': name},
-            {$set {'name': name, 'topics': topics}})
+            {$set {'topics': topics}})
