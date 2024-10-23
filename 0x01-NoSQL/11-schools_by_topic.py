128dd347769d9fde99999625e868468bbda936b7
Peter Odero <peterodero561@gmail.com>
2024-10-24 02:51:29 +0300

    fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,5 @@ from pymongo import MongoClient
 def schools_by_topic(mongo_collection, topic):
     '''returns the list of school having a specific topic'''
     result = list(mongo_collection.find({'topicis': topic}))
-    return result
+    school_names = [school['name'] for school in result]
+    return school_names
