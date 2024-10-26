67ffdba0f19cd31958382c47044fd7c2e6be6456
Peter Odero <peterodero561@gmail.com>
2024-10-26 06:03:23 +0300

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''A class for Cache using redis'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache():
@@ -18,3 +18,23 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Optional[
+                Callable[[bytes], Union[str, int, float, bytes]]] = None
+            ) -> Optional[Union[str, int, float, bytes]]:
+        ''' convert the data back to the desired format.'''
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''returns data in utf-8'''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        '''returns data as int'''
+        return self.get(key, int)
