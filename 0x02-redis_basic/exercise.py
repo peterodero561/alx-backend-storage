fc116d0aad9fe7dd1919dad994c256586df1467e
Peter Odero <peterodero561@gmail.com>
2024-10-26 06:16:50 +0300

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,19 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    '''counts how many times a methods is called'''
+    @wrapper(method)
+    def wrapper(self, *args, **kwargs):
+        '''helper function'''
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache():
@@ -12,6 +25,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''store the input data in Redis using the random key and
         return the key.'''
